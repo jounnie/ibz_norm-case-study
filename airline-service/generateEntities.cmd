82b1f50a6a42acc0699a465629ff1eedddf959b1
Jonathan Ruiz <jonathan@ruiz.io>
2019-08-21 23:12:26 +0200

    remove unnecessary test data record and file

--- airline-service/generateEntities.cmd
+++
@@ -1,7 +0,0 @@
-dotnet ef dbcontext scaffold ^
-Name=DefaultConnection ^
-Microsoft.EntityFrameworkCore.SqlServer ^
---context-dir Entities --output-dir Entities ^
---force ^
---context SpiritDbContext ^
---data-annotations --verbose
